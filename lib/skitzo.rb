dc05c31202e1941b791ae7129a9e233cecd34b65
Jack Dempsey <jack.dempsey@gmail.com>
2010-07-22 16:03:19 -0400

    setup engine requires

---
+++ lib/skitzo.rb
@@ -0,0 +1,3 @@
+module Skitzo
+  require 'skitzo/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
+end
---
+++ lib/skitzo/engine.rb
@@ -0,0 +1,8 @@
+require 'skitzo'
+require 'rails'
+
+module Skitzo
+ class Engine < Rails::Engine
+    engine_name :skitzo
+  end
+end
