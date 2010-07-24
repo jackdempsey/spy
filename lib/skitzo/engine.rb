0d3f54329c6f67cc77c6592158554014fc3d0b4b
Jack Dempsey <jack.dempsey@gmail.com>
2010-07-24 12:36:59 -0400

    add load path for skitzo files

--- lib/skitzo/engine.rb
+++ lib/skitzo/engine.rb
@@ -6,5 +6,8 @@ module Skitzo
     engine_name :skitzo
 
     config.middleware.use Skitzo::Switcher
+
+    # Add a load path for this specific Engine
+    config.load_paths << File.expand_path("../../lib/skitzo", __FILE__)
   end
 end
