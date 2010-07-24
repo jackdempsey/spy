8e0fa38130fa9386f4aaf1eedb4aae7fefd1848e
Jack Dempsey <jack.dempsey@gmail.com>
2010-07-24 12:44:30 -0400

    update engine related code to latest standard

--- lib/skitzo/engine.rb
+++ lib/skitzo/engine.rb
@@ -3,9 +3,10 @@ require 'rails'
 
 module Skitzo
   class Engine < Rails::Engine
-    engine_name :skitzo
 
-    config.middleware.use Skitzo::Switcher
+    initializer "skitzo.add_middleware" do |app|
+      app.middlewares.use Skitzo::Switcher
+    end
 
     # Add a load path for this specific Engine
     config.load_paths << File.expand_path("../../lib/skitzo", __FILE__)
