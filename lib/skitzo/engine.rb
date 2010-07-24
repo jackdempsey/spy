1fe6b57bb655538e88b8108a529392a6f7b36544
Jack Dempsey <jack.dempsey@gmail.com>
2010-07-24 14:02:08 -0400

    require user class to add methods like is_admin? when ready

--- lib/skitzo/engine.rb
+++ lib/skitzo/engine.rb
@@ -8,5 +8,10 @@ module Skitzo
       app.middleware.use Skitzo::Switcher
     end
 
+    config.after_initialize do
+      # make sure user model is loaded before we open up and add methods
+      require 'skitzo/user'
+    end
+
   end
 end
